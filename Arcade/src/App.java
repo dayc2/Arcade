24b139069684303c8c1e6953636d5ba05701e8dc
dayc2 <125522121+dayc2@users.noreply.github.com>
2023-02-16 22:06:33 +0000

    Set up project

---
+++ Arcade/src/App.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World");
+        JFrame frame = new JFrame();
+    }
+}
