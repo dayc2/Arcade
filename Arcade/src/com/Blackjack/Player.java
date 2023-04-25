2e8abec5bc99031995bdf90630d80886e220c047
Gabe761 <125522766+Gabe761@users.noreply.github.com>
2023-04-25 10:08:18 -0400

    Update Player.java

--- Arcade/src/com/Blackjack/Player.java
+++ Arcade/src/com/Blackjack/Player.java
@@ -3,10 +3,25 @@ package com.Blackjack;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.Border;
+
 public class Player {
 
 	private int money = 50;
 	private List<Card> hand = new ArrayList<>();
+	JPanel playerPanel = new JPanel();
+	JLabel nameLabel = new JLabel();
+	JLabel totalLabel = new JLabel();
 	
 	public Player(){
 		//does nothing
@@ -56,4 +71,51 @@ public class Player {
 		
 		return result;
 	}
+	
+	public void makeSpace(JFrame frame, int x, int y, String p) {
+		Border border = BorderFactory.createLineBorder(Color.black);
+		nameLabel.setBounds(10, 269, 160, 81);
+		totalLabel.setBounds(160, 269, 80, 81);
+		playerPanel.setBounds(x, y, 320, 360);
+		playerPanel.setLayout(null);
+		playerPanel.setBorder(border);
+		nameLabel.setText(p);
+		totalLabel.setText("Total: " + getValue());
+		playerPanel.add(nameLabel);
+		playerPanel.add(totalLabel);
+		frame.add(playerPanel);
+		playerPanel.setBackground(Color.green);
+	}
+	
+	public void updateSpace(JFrame frame) {
+		System.out.println("called");
+		JLabel label = new JLabel();
+		if (hand.size() <= 5) {
+			label.setBounds(((hand.size() - 1) * 64), 0, 64, 93);
+		}else if (hand.size() <= 10) {
+			label.setBounds(((hand.size() - 6) * 64), 93, 64, 93);
+		}else {
+			label.setBounds(((hand.size() - 11) * 64), 186, 64, 93);
+		}
+		BufferedImage img = null;
+		System.out.println((hand.get(hand.size() - 1).getURL()));
+		try {
+		    img = ImageIO.read(new File(hand.get(hand.size() - 1).getURL()));
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		Image dimg = img.getScaledInstance(label.getWidth(), label.getHeight(),
+		        Image.SCALE_SMOOTH);
+		ImageIcon image = new ImageIcon(dimg);
+		
+		totalLabel.setText("Total: " + getValue());
+		
+		label.setIcon(image);
+		label.setBackground(Color.blue);
+		playerPanel.add(label);
+		frame.revalidate();
+		frame.repaint();
+		
+	}
+	
 }
