97d25994a64e24c522ce582479e7668df7a02604
dayc2 <125522121+dayc2@users.noreply.github.com>
2023-04-27 17:31:41 -0400

    Update Main.java

--- Arcade/src/Main.java
+++ Arcade/src/Main.java
@@ -1,33 +1,7 @@
 import com.GameSelectionScreen.*;
 
-import java.awt.Color;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-import com.JPanelButton;
-import com.Blackjack.Blackjack;
-
 public class Main {
     public static void main(String[] args){
         new GameSelectionScreen();
-        // JFrame f = new JFrame();
-        // JPanel p = new JPanel();
-        // p.setPreferredSize(new java.awt.Dimension(600, 400));
-        // f.setVisible(true);
-        // JPanelButton panel = new JPanelButton("Message", Color.WHITE){
-        //     @Override
-        //     public void onClick() {
-        //         System.out.println("Clicked");
-        //     }
-        // };
-        // panel.setPreferredSize(new java.awt.Dimension(100, 100));
-        // f.add(p);
-        // p.add(panel);
-        // p.setBackground(Color.RED);
-        // f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // f.pack();
     }
 }
